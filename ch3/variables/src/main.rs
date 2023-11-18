d9ce9d4b6cc239b9a3ece14207d1db78d5f10171
Gonzalo Campos <gonzalocampose1@gmail.com>
2023-11-18 11:36:21 -0600

    Number operations

--- ch3/variables/src/main.rs
+++ ch3/variables/src/main.rs
@@ -16,5 +16,14 @@ fn main() {
     // floating point numbers
     let x = 2.0; // f64 by default
     let y: f32 = 3.0; // f32
+    
+    let sum = 5 + 10;
+    let difference = 95.5 4.3;
+    let product = 4 * 30;
+
+    let quotiend = 56.7 / 32.2;
+    let truncated = -5 / 3; // Results in -1
+
+    let remainder = 43 % 5;
 }
 
