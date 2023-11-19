0e4f194078a58a2a0c064931bcda6e112ea226de
Gonzalo Campos <gonzalocampose1@gmail.com>
2023-11-19 16:08:40 -0600

    Statements and expressions

--- ch3/functions/src/main.rs
+++ ch3/functions/src/main.rs
@@ -1,6 +1,20 @@
 fn main() {
     another_function(5);
     print_labeled_measurement(5, 'h');
+
+    let y = 6; // statement
+
+    let y = {
+        let x = 3;
+        // expressions do not include ending semicolons
+        // if it's added, the expression will be turned
+        // into a statement
+        x + 1
+    }; // new scopes are expressions
+    
+    let x = five();
+    let x = plus_one(x);
+    println!("The value of x is: {x}");
 }
 
 fn another_function(x: i32) {
@@ -11,3 +25,10 @@ fn print_labeled_measurement(value: i32, unit_label: char) {
     println!("The measurement is: {value}{unit_label}");
 }
 
+fn plus_one(x: i32) -> i32 {
+    x + 1
+}
+
+fn five() -> i32 {
+    5
+}
